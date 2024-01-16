3dfa5219c69591edca905f5e170462f029454d73
mmateo <mateo.mesguen@ynov.com>
2024-01-16 08:57:59 +0100

    push bdd

--- bdd/database.sql
+++ bdd/database.sql
@@ -1,24 +1,59 @@
-CREATE TABLE user (
-    mail VARCHAR(100) PRIMARY KEY NOT NULL,
-    status VARCHAR(100) NOT NULL,
-    hash VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE hero (
+-- Table de base pour héro
+CREATE TABLE Hero (
     hero_id INT PRIMARY KEY AUTO_INCREMENT,
-    firstname VARCHAR(255) NOT NULL,
-    lastname VARCHAR(255) NOT NULL,
-    sector VARCHAR(255) NOT NULL,
-    module VARCHAR(255) NOT NULL
+    nom VARCHAR(255) NOT NULL,
+    identite_secrete VARCHAR(255) NOT NULL,
+    sexe ENUM('Masculin', 'Féminin') NOT NULL,
+    couleur_cheveux VARCHAR(50),
+    planete_origine VARCHAR(255),
+    descriptio TEXT,
+    UNIQUE KEY (nom, identite_secrete)
 );
 
-CREATE TABLE poll (
-    poll_id INT PRIMARY KEY AUTO_INCREMENT,
-    fk_user_mail VARCHAR(100) NOT NULL,
-    fk_id_teacher INT NOT NULL,
-    score INT NOT NULL,
-    comment TEXT NOT NULL,
-    report BOOLEAN,
-    FOREIGN KEY (fk_user_mail) REFERENCES user(mail),
-    FOREIGN KEY (fk_id_hero) REFERENCES hero(hero_id)
+CREATE TABLE Superpouvoir (
+    pouvoir_id INT PRIMARY KEY AUTO_INCREMENT,
+    pouvoir_nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Ville (
+    ville_id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Gadget (
+    gadget_id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Coequipier (
+    coequipier_id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Vehicule (
+    vehicule_id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Groupe (
+    groupe_id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE HeroRelation (
+    hero_id INT,
+    pouvoir_id INT,
+    ville_id INT,
+    gadget_id INT,
+    coequipier_id INT,
+    vehicule_id INT,
+    groupe_id INT,
+    PRIMARY KEY (hero_id),
+    FOREIGN KEY (hero_id) REFERENCES Hero(hero_id),
+    FOREIGN KEY (pouvoir_id) REFERENCES Superpouvoir(pouvoir_id),
+    FOREIGN KEY (ville_id) REFERENCES Ville(ville_id),
+    FOREIGN KEY (gadget_id) REFERENCES Gadget(gadget_id),
+    FOREIGN KEY (coequipier_id) REFERENCES Coequipier(coequipier_id),
+    FOREIGN KEY (vehicule_id) REFERENCES Vehicule(vehicule_id),
+    FOREIGN KEY (groupe_id) REFERENCES Groupe(groupe_id)
 );
