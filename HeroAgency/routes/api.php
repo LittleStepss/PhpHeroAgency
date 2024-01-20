9cd26bfff03c9a4faeb063f1609dedac451d3de0
mmateo <mateo.mesguen@ynov.com>
2024-01-20 12:57:03 +0100

    Push API

--- HeroAgency/routes/api.php
+++ HeroAgency/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HeroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/hero',[HeroController::class, 'index']);
+Route::get('/hero/{id}', [HeroController::class, 'show']);
+Route::post('/hero', [HeroController::class, 'store']);
+Route::put('/hero/{id}', [HeroController::class, 'update']);
+Route::delete('/hero/{id}', [HeroController::class, 'destroy']);
