d095ffa0e53def6e544582fd5283d2617c4b5648
mmateo <mateo.mesguen@ynov.com>
2024-01-20 20:58:42 +0100

    push seeder update and html update

--- HeroAgency/database/seeders/HeroSeeder.php
+++ HeroAgency/database/seeders/HeroSeeder.php
@@ -37,5 +37,53 @@ class HeroSeeder extends Seeder
             'hair_color' => 'Brun',
             'original_planet' => 'Terre',
         ]);
+
+        Hero::create([
+            'name' => 'Hulk',
+            'secret_identity' => 'Bruce Banner',
+            'gender' => 'Mâle',
+            'hair_color' => 'Brun',
+            'original_planet' => 'Terre',
+        ]);
+
+        Hero::create([
+            'name' => 'MM',
+            'secret_identity' => 'Mateo Mesguen',
+            'gender' => 'Mâle',
+            'hair_color' => 'Blond',
+            'original_planet' => 'Terre',
+        ]);
+
+        Hero::create([
+            'name' => 'Thor',
+            'secret_identity' => 'Thor',
+            'gender' => 'Mâle',
+            'hair_color' => 'Blond',
+            'original_planet' => 'Asgard',
+        ]);
+
+        Hero::create([
+            'name' => 'Naruto',
+            'secret_identity' => 'Naruto Uzumaki',
+            'gender' => 'Mâle',
+            'hair_color' => 'Blond',
+            'original_planet' => 'Konoha',
+        ]);
+
+        Hero::create([
+            'name' => 'IronMan',
+            'secret_identity' => 'Tony Stark',
+            'gender' => 'Mâle',
+            'hair_color' => 'Brun',
+            'original_planet' => 'Terre',
+        ]);
+
+        Hero::create([
+            'name' => 'Wonderwoman',
+            'secret_identity' => 'Diana Prince',
+            'gender' => 'Femelle',
+            'hair_color' => 'Brune',
+            'original_planet' => 'Themyscira',
+        ]);
     }
 }
